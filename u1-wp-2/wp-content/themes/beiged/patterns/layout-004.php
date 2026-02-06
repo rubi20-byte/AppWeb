cf38cd709ea7bf4375f0a737c75bfd9210a2bac4
rubi20-byte <2330317@upv.edu.mx>
2026-02-05 22:44:20 -0600

    Subiendo WordPress limpio

---
+++ u1-wp-2/wp-content/themes/beiged/patterns/layout-004.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Title: Layout 004
+ * Slug: beiged/layout-004
+ * Categories: layouts
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|superbspacing-xlarge","bottom":"var:preset|spacing|superbspacing-xlarge","left":"var:preset|spacing|superbspacing-small","right":"var:preset|spacing|superbspacing-small"},"blockGap":"var:preset|spacing|superbspacing-small","margin":{"top":"0","bottom":"0"}}},"className":"column-parent-border-radius","layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group alignfull column-parent-border-radius" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--superbspacing-xlarge);padding-right:var(--wp--preset--spacing--superbspacing-small);padding-bottom:var(--wp--preset--spacing--superbspacing-xlarge);padding-left:var(--wp--preset--spacing--superbspacing-small)"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|superbspacing-xxsmall"}},"layout":{"type":"constrained","contentSize":"600px"}} -->
+  <div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"center","level":3,"textColor":"primary","fontSize":"superbfont-small"} -->
+    <h3 class="wp-block-heading has-text-align-center has-primary-color has-text-color has-superbfont-small-font-size"><?php esc_html_e("Easy customizable", "beiged"); ?></h3>
+    <!-- /wp:heading -->
+
+    <!-- wp:heading {"textAlign":"center","textColor":"contrast","fontSize":"superbfont-xlarge"} -->
+    <h2 class="wp-block-heading has-text-align-center has-contrast-color has-text-color has-superbfont-xlarge-font-size"><?php esc_html_e("A Beautiful Foundation", "beiged"); ?>
+    </h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"align":"center","textColor":"mono-2","fontSize":"superbfont-small"} -->
+    <p class="has-text-align-center has-mono-2-color has-text-color has-superbfont-small-font-size"><?php esc_html_e("Create a calm, minimal website that adapts to your needs. Beiged is built to help anyone design a site that feels professional and natural.", "beiged"); ?></p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:columns -->
+  <div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|superbspacing-small","bottom":"var:preset|spacing|superbspacing-small","left":"var:preset|spacing|superbspacing-small","right":"var:preset|spacing|superbspacing-small"}}},"backgroundColor":"mono-4"} -->
+    <div class="wp-block-column has-mono-4-background-color has-background" style="padding-top:var(--wp--preset--spacing--superbspacing-small);padding-right:var(--wp--preset--spacing--superbspacing-small);padding-bottom:var(--wp--preset--spacing--superbspacing-small);padding-left:var(--wp--preset--spacing--superbspacing-small)"><!-- wp:image {"align":"center","width":"48px","height":"48px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+      <figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url(get_stylesheet_directory_uri() . "/inc/superbthemes-info-content/icons/img-icon-7.png"); ?>" alt="" style="object-fit:cover;width:48px;height:48px" /></figure>
+      <!-- /wp:image -->
+
+      <!-- wp:heading {"textAlign":"center","level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|mono-1"}}}},"textColor":"mono-1","fontSize":"superbfont-medium"} -->
+      <h3 class="wp-block-heading has-text-align-center has-mono-1-color has-text-color has-link-color has-superbfont-medium-font-size" style="margin-top:0;margin-bottom:0"><?php esc_html_e("Customization", "beiged"); ?></h3>
+      <!-- /wp:heading -->
+
+      <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|superbspacing-xxsmall","bottom":"0"}}},"textColor":"mono-2","fontSize":"superbfont-xsmall"} -->
+      <p class="has-text-align-center has-mono-2-color has-text-color has-superbfont-xsmall-font-size" style="margin-top:var(--wp--preset--spacing--superbspacing-xxsmall);margin-bottom:0"><?php esc_html_e("Every element can be adjusted to fit your vision. Change fonts, colors, and layouts quickly, no coding needed.", "beiged"); ?></p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:column -->
+
+    <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|superbspacing-small","bottom":"var:preset|spacing|superbspacing-small","left":"var:preset|spacing|superbspacing-small","right":"var:preset|spacing|superbspacing-small"}}},"backgroundColor":"mono-4"} -->
+    <div class="wp-block-column has-mono-4-background-color has-background" style="padding-top:var(--wp--preset--spacing--superbspacing-small);padding-right:var(--wp--preset--spacing--superbspacing-small);padding-bottom:var(--wp--preset--spacing--superbspacing-small);padding-left:var(--wp--preset--spacing--superbspacing-small)"><!-- wp:image {"align":"center","width":"48px","height":"48px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+      <figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url(get_stylesheet_directory_uri() . "/inc/superbthemes-info-content/icons/img-icon-5.png"); ?>" alt="" style="object-fit:cover;width:48px;height:48px" /></figure>
+      <!-- /wp:image -->
+
+      <!-- wp:heading {"textAlign":"center","level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|mono-1"}}}},"textColor":"mono-1","fontSize":"superbfont-medium"} -->
+      <h3 class="wp-block-heading has-text-align-center has-mono-1-color has-text-color has-link-color has-superbfont-medium-font-size" style="margin-top:0;margin-bottom:0"><?php esc_html_e("eCommerce", "beiged"); ?></h3>
+      <!-- /wp:heading -->
+
+      <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|superbspacing-xxsmall","bottom":"0"}}},"textColor":"mono-2","fontSize":"superbfont-xsmall"} -->
+      <p class="has-text-align-center has-mono-2-color has-text-color has-superbfont-xsmall-font-size" style="margin-top:var(--wp--preset--spacing--superbspacing-xxsmall);margin-bottom:0"><?php esc_html_e("Beiged is fully compatible with WooCommerce, giving you a simple way to create an online store in minutes.", "beiged"); ?></p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:column -->
+
+    <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|superbspacing-small","bottom":"var:preset|spacing|superbspacing-small","left":"var:preset|spacing|superbspacing-small","right":"var:preset|spacing|superbspacing-small"}}},"backgroundColor":"mono-4"} -->
+    <div class="wp-block-column has-mono-4-background-color has-background" style="padding-top:var(--wp--preset--spacing--superbspacing-small);padding-right:var(--wp--preset--spacing--superbspacing-small);padding-bottom:var(--wp--preset--spacing--superbspacing-small);padding-left:var(--wp--preset--spacing--superbspacing-small)"><!-- wp:image {"align":"center","width":"48px","height":"48px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+      <figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url(get_stylesheet_directory_uri() . "/inc/superbthemes-info-content/icons/img-icon-6.png"); ?>" alt="" style="object-fit:cover;width:48px;height:48px" /></figure>
+      <!-- /wp:image -->
+
+      <!-- wp:heading {"textAlign":"center","level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"mono-1","fontSize":"superbfont-medium"} -->
+      <h3 class="wp-block-heading has-text-align-center has-mono-1-color has-text-color has-superbfont-medium-font-size" style="margin-top:0;margin-bottom:0"><?php esc_html_e("Lightning Fast", "beiged"); ?></h3>
+      <!-- /wp:heading -->
+
+      <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|superbspacing-xxsmall","bottom":"0"}}},"textColor":"mono-2","fontSize":"superbfont-xsmall"} -->
+      <p class="has-text-align-center has-mono-2-color has-text-color has-superbfont-xsmall-font-size" style="margin-top:var(--wp--preset--spacing--superbspacing-xxsmall);margin-bottom:0"><?php esc_html_e("Optimized for speed and clarity. Beiged loads quickly and provides a smooth experience across all modern devices.", "beiged"); ?></p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:column -->
+  </div>
+  <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
